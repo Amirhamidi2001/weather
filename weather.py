c423e2612577058abcaad7d7a038a8e75438b811
Amir Arsalan Hamidi <78895407+Amirhamidi2001@users.noreply.github.com>
2022-02-01 12:14:44 +0330

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  1 11:02:13 2022
+
+@author: lenovo
+"""
+
+import requests
+import datetime
+import sqlite3
+
+def proccess_data(data):
+    return [data['name'],
+            data['sys']['country'],
+            data['weather'][0]['main'],
+            data['weather'][0]['icon']]
+
+
+def weather(lat = '35.7', lon = '51.4', appid = '0ba870d2feeb31ce3e65df98f16623ff'):
+    url = 'http://api.openweathermap.org/data/2.5/weather?'
+    r = requests.get(url+'lat='+lat+'&lon='+lon+'&appid='+appid)
+    return proccess_data(r.json())
+
+
+def date():
+    data = (datetime.date.isoformat(datetime.date.today()))
+    return data
+
+
+def sql_connector(path):
+    cnx = sqlite3.connect(path)
+    corsor = cnx.cursor()
+    return cnx, corsor
+
+
+def create_table(cnx, cursor):
+    query = 'CREATE TABLE IF NOT EXISTS btc (date text, city text, country text, main text, icon text);'
+    cursor.execute(query)
+    cnx.commit()
+
+
+def insert_data(date, city, country, main, icon):
+    query = "INSERT INTO btc VALUES (\'%s\', \'%s\', \'%s\', \'%s\', \'%s\')"%(date,
+                                                                               city,
+                                                                               country,
+                                                                               main,
+                                                                               icon)
+    cursor.execute(query)
+    cnx.commit()
+
+
+path = 'Bitcoin.db'
+cnx, cursor = sql_connector(path)
+create_table(cnx, cursor)
+date = date()
+city, country, main, icon = weather('35', '51')
+insert_data(date, city, country, main, icon)
